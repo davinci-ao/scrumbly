f716c27b5afdebb4c73f8274b6fd23d3c4f908d5
kevin <koevoen420@gmail.com>
2021-11-16 11:34:43 +0100

    Created base for crud for feature (add and display)

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -12,6 +12,12 @@
 
     @include("layouts.sprint-form")
 
+    <a data-toggle="modal" data-target="#addNewFeature" class="text-decoration-none ml-auto font-semibold  hover:text-gray-600 text-xl text-gray-800 leading-tight">
+        {{ __('Add a new Feature') }}
+    </a>
+
+    @include('layouts.feature-form')
+
     <a data-toggle="modal" data-target="#addNewSprint" class="text-decoration-none ml-auto font-semibold  hover:text-gray-600 text-xl text-gray-800 leading-tight">
         {{ __('Add a new Sprint') }}
     </a>
@@ -22,6 +28,19 @@
             @if($sprints != null)
                 @foreach($sprints as $sprint)
                     @include('layouts.sprint', ['sprint' => $sprint])
+                @endforeach
+            @endif
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg row">
+            <h3 class="m-3">User stories</h3>
+            @if($features != null)
+                @foreach($features as $feature)
+                    @include('layouts.feature', ['feature' => $feature])
                 @endforeach
             @endif
             </div>
