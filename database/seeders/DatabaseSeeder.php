98f7315d3fdc576b892478a6d72f616962529623
Xeno <xenocidechan@gmail.com>
2021-11-16 11:08:56 +0100

    Added rights

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('rights')->insert([
+            'name' => 'admin',
+        ]);
     }
 }
