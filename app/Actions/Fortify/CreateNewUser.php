ed336a7fc3323993125825e90116e755a52551c6
Xeno <xenocidechan@gmail.com>
2021-11-17 11:06:07 +0100

    final commit to fix merge issue

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -34,7 +34,7 @@ class CreateNewUser implements CreatesNewUsers
                 'name' => $input['name'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
-                'rights' => 'user',
+                'rights' => 'user', // remove this comment
             ]), function (User $user) {
                 $this->createTeam($user);
             });
