216f0b7129f1998ec7b11cb3af08a3617cae1680
Xeno <42929153+Xenocidious@users.noreply.github.com>
2021-11-17 10:28:40 +0100

    Update delete-feature.blade.php

--- resources/views/layouts/delete-feature.blade.php
+++ resources/views/layouts/delete-feature.blade.php
@@ -2,7 +2,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Weetje zeker dat je deze {{ $feature->name }} wilt verwijderen?</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Weet je zeker dat je {{ $feature->name }} wilt verwijderen?</h5>
       </div>
       <div class="modal-body">
         
