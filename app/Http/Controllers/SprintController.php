9def442b822499bcf2f2f0f568da787084468d38
Xeno <42929153+Xenocidious@users.noreply.github.com>
2021-11-18 10:10:50 +0100

    Update SprintController.php

--- app/Http/Controllers/SprintController.php
+++ app/Http/Controllers/SprintController.php
@@ -30,7 +30,7 @@ class SprintController extends Controller
 
     public function finish(Request $request)
     {
-        $sprint = Sprint::find($request->input('sprintId'));
+        $sprint = Sprint::find($request->input('sprint_id'));
         $sprint->status_id = 1;
         $sprint->save();
 
