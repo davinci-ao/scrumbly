0610ced50783276e88041e7cde3b907b3e6e5f10
kevin16702 <40112089+kevin16702@users.noreply.github.com>
2021-11-18 10:11:27 +0100

    Fixed finish function (#9)
    * Fixed finish function
    
    * Update sprint.blade.php
    
    * Update SprintController.php
    
    Co-authored-by: Xeno <42929153+Xenocidious@users.noreply.github.com>

--- resources/views/layouts/sprint-form.blade.php
+++ resources/views/layouts/sprint-form.blade.php
@@ -9,7 +9,7 @@
         @csrf
           <div class="form-group">
             <label for="playlist-name" class="col-form-label">Sprint name:</label>
-            <input type="text" class="form-control" id="name" name="name">
+            <input type="text" class="form-control" id="name" name="name" required>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
