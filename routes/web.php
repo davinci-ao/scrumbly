9f0c69d33dd3d6bb30a00a28bd25c9ad004c0b51
kevin <koevoen420@gmail.com>
2021-11-25 11:53:42 +0100

    Added email verification

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [ProjectsController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [ProjectsController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/overview', [SprintController::class, 'index'])->middleware(['auth'])->name('projects');
+Route::get('/overview', [SprintController::class, 'index'])->middleware(['auth', 'verified'])->name('projects');
 
 Route::post('/overview/delete/{feature_id}', [FeatureController::class, 'deleteFeature'])->name('deleteFeature');
-Route::get('/overview/edit/{feature_id}', [FeatureController::class, 'editFeature'])->middleware(['auth'])->name('editFeature');
-Route::get('/overview/push', [SprintController::class, 'push'])->middleware(['auth'])->name('addSprint');
-Route::get('/overview/push-feature', [FeatureController::class, 'push'])->middleware(['auth'])->name('addFeature');
-Route::get('/overview/finish', [SprintController::class, 'finish'])->middleware(['auth'])->name('finishSprint');
+Route::get('/overview/edit/{feature_id}', [FeatureController::class, 'editFeature'])->middleware(['auth', 'verified'])->name('editFeature');
+Route::get('/overview/push', [SprintController::class, 'push'])->middleware(['auth', 'verified'])->name('addSprint');
+Route::get('/overview/push-feature', [FeatureController::class, 'push'])->middleware(['auth', 'verified'])->name('addFeature');
+Route::get('/overview/finish', [SprintController::class, 'finish'])->middleware(['auth', 'verified'])->name('finishSprint');
 
 require __DIR__.'/auth.php';
