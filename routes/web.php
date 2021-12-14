c6f42943b196491d46180d09c5073d17c6d7c5ee
kevin16702 <40112089+kevin16702@users.noreply.github.com>
2021-12-14 10:22:16 +0100

    Mege dev with main (#18)
    * changed function naming (#16)
    
    * changed function naming
    
    * Update DashboardController.php
    
    * Update PanelController.php
    
    Authored-by: NIlsSchnitzel <235770@student.insulacollege.nl>
    Co-authored-by: Xeno <42929153+Xenocidious@users.noreply.github.com>
    
    * change edituser type to email (#17)
    
    Authored-by: NIlsSchnitzel <235770@student.insulacollege.nl>
    
    * hotfix/disable email verification
    
    Co-authored-by: Nils <71555694+NilsBioch@users.noreply.github.com>
    Co-authored-by: Xeno <42929153+Xenocidious@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,16 @@ Route::get('/', function () {
 });
 
 Route::get('/homepage', [ProjectController::class, 'index'])->middleware(['auth'])->name('homepage');
-Route::get('/projects', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('projects');
-Route::get('/project/{project_id}', [ProjectController::class, 'projectIndex'])->middleware(['auth', 'verified'])->name('projectIndex');
-Route::post('/project/finishPanel/{panel_id}', [PanelController::class, 'finishPanel'])->middleware(['auth', 'verified'])->name('finishPanel');
-Route::post('/project/createPanel', [PanelController::class, 'createPanel'])->middleware(['auth', 'verified'])->name('createPanel');
-Route::post('/project/editPanel/{panel_id}', [PanelController::class, 'editPanel'])->middleware(['auth', 'verified'])->name('editPanel');
-Route::post('/project/deletePanel/{panel_id}', [PanelController::class, 'deletePanel'])->middleware(['auth', 'verified'])->name('deletePanel');
+Route::get('/projects', [ProjectController::class, 'index'])->middleware(['auth'])->name('projects');
+Route::get('/project/{project_id}', [ProjectController::class, 'projectIndex'])->middleware(['auth'])->name('projectIndex');
+Route::post('/project/finishPanel/{panel_id}', [PanelController::class, 'finishPanel'])->middleware(['auth'])->name('finishPanel');
+Route::post('/project/createPanel', [PanelController::class, 'createPanel'])->middleware(['auth'])->name('createPanel');
+Route::post('/project/editPanel/{panel_id}', [PanelController::class, 'editPanel'])->middleware(['auth'])->name('editPanel');
+Route::post('/project/deletePanel/{panel_id}', [PanelController::class, 'deletePanel'])->middleware(['auth'])->name('deletePanel');
 
-Route::post('/project/createFeature', [FeatureController::class, 'createFeature'])->middleware(['auth', 'verified'])->name('createFeature');
-Route::post('/project/deleteFeature/{feature_id}', [FeatureController::class, 'deleteFeature'])->middleware(['auth', 'verified'])->name('deleteFeature');
-Route::post('/project/editFeature/{feature_id}', [FeatureController::class, 'editFeature'])->middleware(['auth', 'verified'])->name('editFeature');
+Route::post('/project/createFeature', [FeatureController::class, 'createFeature'])->middleware(['auth'])->name('createFeature');
+Route::post('/project/deleteFeature/{feature_id}', [FeatureController::class, 'deleteFeature'])->middleware(['auth'])->name('deleteFeature');
+Route::post('/project/editFeature/{feature_id}', [FeatureController::class, 'editFeature'])->middleware(['auth'])->name('editFeature');
 
 Route::get('/userlist', [DashboardController::class, 'index'])->middleware(['auth'])->name('userlist');
 Route::get('/userlist/edit/{user_id}', [DashboardController::class, 'edit'])->middleware(['auth'])->name('editUser');
