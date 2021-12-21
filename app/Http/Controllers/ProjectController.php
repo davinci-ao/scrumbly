267394755a56644a1b5aa6b615aa937e6d6ff473
Nils Bioch <99055617@mydavinci.nl>
2021-12-21 13:46:31 +0100

    added a few docblock comments

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,9 +10,9 @@ use App\Models\Feature;
 class ProjectController extends Controller
 {
     /**
-     * Undocumented function
+     * index, collects all the projects and returns to homepage
      *
-     * @return void
+     * @return view
      */
     public function index()
     {
@@ -21,6 +21,12 @@ class ProjectController extends Controller
         return view('homepage', ['projects' => $projects]);
     }
 
+    /**
+     * projectIndex, collects all the projects and returns to the project page
+     *
+     * @param [int] $project_id
+     * @return view
+     */
     public function projectIndex($project_id){
         $project = Project::find($project_id);
         $panels = $project->panels;
@@ -29,6 +35,12 @@ class ProjectController extends Controller
         return view('overview', compact(['project', 'panels', 'features']));
     }
 
+    /**
+     * create, creates a project and automatically generates 3 panels
+     *
+     * @param Request $request
+     * @return view
+     */
     public function create(Request $request){
         $project = new Project;
         $panel = new Panel;
